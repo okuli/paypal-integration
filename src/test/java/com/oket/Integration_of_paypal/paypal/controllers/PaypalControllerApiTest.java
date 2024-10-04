3fb05154750d6c9b7f7d90e01b84127740446994
okuli <oket.hoxha@gmail.com>
2024-10-04 23:58:02 +0200

    API and Integration test implemented. The unit test still is to be fixed.

--- src/test/java/com/oket/Integration_of_paypal/paypal/controllers/PaypalControllerApiTest.java
+++ src/test/java/com/oket/Integration_of_paypal/paypal/controllers/PaypalControllerApiTest.java
@@ -1,28 +1,20 @@
 package com.oket.Integration_of_paypal.paypal.controllers;
 
-import com.oket.Integration_of_paypal.paypal.controllers.PaypalController;
+
 import com.oket.Integration_of_paypal.paypal.services.PaypalService;
-import com.paypal.base.rest.PayPalRESTException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -54,6 +46,6 @@ public class PaypalControllerApiTest {
                         .header("Accept", MediaType.APPLICATION_JSON_VALUE)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) jsonPath("$.approvalUrl").value("http://approval.url"));
+                .andExpect(jsonPath("$.approvalUrl").value("http://approval.url"));  // Fixed the cast issue
     }
 }
